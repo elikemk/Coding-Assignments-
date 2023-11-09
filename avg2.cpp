8b530b6f4317f840cfdd49eb98324b811e86f2cd
Elikem Kalitsi <101428026+elikemk@users.noreply.github.com>
2023-11-09 14:20:16 -0500

    Update avg2.cpp

--- avg2.cpp
+++ avg2.cpp
@@ -3,4 +3,30 @@
 #include <string>
 #include <ctime>
 
+using namespace std;
+
+int main()
+{
+
+srand((unsigned)time(0));
+
+
+int years;
+int grass [12][years];
+int trees[12][years];
+int weeds [12][years];
+int total [12][ years];
+
+string treelevel; 
+string weedlevel;
+string grasslevel;
+
+cout << "Enter the number of Years: ";
+cin >> years;
+cout << endl;
+
+
+
+}
+
 
